a092446c79d847b6cc1a5c46b0482254146ab6be
llxodz <llxodz@gmail.com>
2022-06-07 01:26:15 +0300

    Initial project

---
+++ CleanSwiftExample/CleanSwiftExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CleanSwiftExample
+//
+//  Created by Илья on 07.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
